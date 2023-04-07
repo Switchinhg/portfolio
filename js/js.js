c017200d6993f010716bb8ab14bd9f3a82fe46a3
Santiago <santifonlop@hotmail.com>
2023-04-07 15:09:13 -0300

    agregada funcion para animaciones random

--- js/js.js
+++ js/js.js
@@ -170,6 +170,26 @@
 let openNav = document.getElementById('openNav')
 let tabNav = document.getElementById('tabNav')
 let closeNav = document.getElementById('closeNav')
+const botonVerMas = document.getElementById('botonVerMas')
+/* Pantalla de inicio Santiago Font etc */
+const main = document.getElementById('main')
+/* boton inicio en el navbar */
+const inicioo = document.getElementById('inicioo')
+const proyectosMenu = document.getElementById('proyectosMenu')
+const proyectos = document.getElementById('proyectos')
+const botonSobreMI = document.getElementById('botonSobreMI')
+const sobreMi = document.getElementById('sobreMi')
+
+/* Habilidades  */
+const habilidades = document.getElementById("habilidades")
+/* Contacto */
+const contacto = document.getElementById("contacto")
+
+
+const arregloSecciones = [main, proyectos, sobreMi,habilidades,contacto]
+const aparecer = ['aparecer','aparecer2','aparecer3','aparecer4','aparecer5']
+const desaparecer = ['desaparecer', 'desaparecer2']
+
 
 openNav.addEventListener('click', ()=>{
   tabNav.classList.add('navOpen')
@@ -183,3 +203,39 @@ window.addEventListener('click', (e)=>{
     tabNav.classList.remove('navOpen');
   } 
 })
+
+/* Cerrar todas */
+function cerrarTodasExcepto(noCerrar){
+  if(!noCerrar.classList.contains('open')){
+
+  arregloSecciones.forEach(seccion=>{
+    if(seccion !== noCerrar){
+      aparecer.forEach(e=>seccion.classList.remove(e))
+        seccion.classList.add(getRandomAnimation('desaparecer'))
+        seccion.classList.remove('open')
+      setTimeout(() => {
+
+        seccion.style.display = 'none'
+      }, 1000);
+    }else{
+      tabNav.classList.remove('navOpen');
+      setTimeout(() => {
+        noCerrar.style.display = 'flex'
+        noCerrar.classList.add(getRandomAnimation('aparecer'))
+        desaparecer.forEach(e=>noCerrar.classList.remove(e))
+
+        noCerrar.classList.add('open')
+      }, 1000);
+    }
+  })
+}
+
+}
+
+const getRandomAnimation = (tipo) =>{
+  if(tipo === 'aparecer'){
+    return aparecer[Math.floor(Math.random() * aparecer.length)]
+  }else{
+    return desaparecer[Math.floor(Math.random() * desaparecer.length)]
+  }
+}
